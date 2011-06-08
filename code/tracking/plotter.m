2588ff7b74ccca14acba1ecfa24e4f4b38432980
Matthew Sottile <mjsottile@me.com>
2011-06-08 00:05:37 -0700

    comments to make sure code is understandable later

--- code/tracking/plotter.m
+++ code/tracking/plotter.m
@@ -1,16 +1,33 @@
+% seed with a blank row to get started - we clip this off later
 lines = [0 0];
 
+% get dimensions of adjacency matrix
 [nrows,ncols] = size(adj);
+
+% iterate through adjacency matrix
 for i=1:nrows
+    % if the current row has a nonzero, do something.  Otherwise, skip.
     if (nnz(adj(i,:))>0)
+        % find which columns are nonzero.  this defines the
+        % edges in the graph from vertex i to the set of vertices j
+        % that it is connected to.
         [~,indices] = find(adj(i,:));
+        
+        % find the smallest entry in the row, corresponding to the
+        % edge of least weight (in this case, weight = euclidean distance)
         [~,minidx] = min(nonzeros(adj(i,:)));
+        
+        % convert the sparse 1x1 to a regular 1x1 vector
         didx = full(indices(minidx));
+        
+        % append to the list of valid edges
         lines = [lines; i didx];
     end
 end
 
+% clip off the blank row we used to seed the list
 lines = lines(2:end,:);
+
 
 [nlines,~] = size(lines);
 figure;
