6caa24f5c828eebc7695449a867db008136018e4
Matthew Sottile <mjsottile@me.com>
2011-06-07 23:46:33 -0700

    edits

--- code/tracking/tracker.m
+++ code/tracking/tracker.m
@@ -6,18 +6,21 @@ function [xs,ys,zs,adj] = tracker(ims, thresh, areaCutoff)
   numim = length(ims);
   
   % split images in half
+  [~,imwidth] = size(ims{1});
   for i=1:numim
-      imLeft{i} = ims{i}(:,1:168);
-      imRight{i} = ims{i}(:,169:end);
+    imLeft{i} = ims{i}(:,1:(imwidth/2));
+    imRight{i} = ims{i}(:,((imwidth/2)+1):end);
   end
   
   % binarize images
   for i=1:numim
       % binarize right side with threshold
-      binIms{i} = imRight{i} > thresh;
+      binIms{i} = imLeft{i} > thresh;
       
       % plot
       subplot(1,2,1);imagesc(binIms{i});
+      se = strel('disk',3);        
+      binIms{i} = imerode(binIms{i},se);
             
       % label connected components, compute their area
       lbl = bwlabel(binIms{i});
@@ -53,6 +56,8 @@ function [xs,ys,zs,adj] = tracker(ims, thresh, areaCutoff)
   
       % store centroid
       cents{i} = centroids;
+      
+      disp(100*(i/numim));
 
       drawnow;
   end
@@ -60,10 +65,11 @@ function [xs,ys,zs,adj] = tracker(ims, thresh, areaCutoff)
   
   % triangulation stuff
   
-  multiplier = 5;
+  multiplier = 0.5;
+
   xs = cents{1}(1,:);
   ys = cents{1}(2,:);
-  zs = ones(1,length(cents{1}(1,:)));
+  zs = ones(1,length(cents{1}(1,:))).*multiplier;
   
   for i=2:numim
     if (~isempty(cents{i}))
@@ -73,10 +79,6 @@ function [xs,ys,zs,adj] = tracker(ims, thresh, areaCutoff)
     end
   end  
   
-%  close all;
-%  plot3(xs,ys,zs,'.');
-%  drawnow;
-  
   tri = delaunay(xs,ys,zs);
   
   % tets to lines
@@ -84,32 +86,43 @@ function [xs,ys,zs,adj] = tracker(ims, thresh, areaCutoff)
          tri(:,2) tri(:,3); ...
          tri(:,3) tri(:,4); ...
          tri(:,4) tri(:,1)];
-       
+  
+  % want to make sure lines are sorted such that the first point is always
+  % of a lower Z value than the second point
+
+  p1z = zs(tri(:,1));
+  p2z = zs(tri(:,2));
+  
+  mask = p1z < p2z;
+  notmask = not(mask);
+
+  tri = [tri(mask,:);
+         fliplr(tri(notmask,:))];
+
+  % now, sort the lines in ascending order of z coordinate of first point
+  tzs = zs(tri(:,1));
+  [~,indices] = sort(tzs);
+  tri = tri(indices,:);
+
+  % ok, now we're set up with an array of lines that is tuned for quickly
+  % creating the sparse adjacency matrix.
   lines = [0 0];
 
   [nsegs,~] = size(tri);
   
-  adj = sparse(numim,numim);
+  adj = sparse(1,1);
   
-  h = waitbar(0);
-  for i=1:nsegs
-    na=tri(i,1);
-    xa=xs(tri(i,1));
-    ya=ys(tri(i,1));
-    za=zs(tri(i,1));
-    nb=tri(i,2);
-    xb=xs(tri(i,2));
-    yb=ys(tri(i,2));
-    zb=zs(tri(i,2));    
-    
-    if (za ~= zb)
-        if (za < zb)
-            adj(na,nb) = sqrt((xa-xb)^2 + (ya-yb)^2 + (za-zb)^2);
-        else
-            adj(nb,na) = sqrt((xa-xb)^2 + (ya-yb)^2 + (za-zb)^2);
-        end
-    end
-    
-    waitbar(i/nsegs);
+  na = tri(:,1);
+  nb = tri(:,2);
+  xa = xs(na);
+  ya = ys(na);
+  za = zs(na);
+  xb = xs(nb);
+  yb = ys(nb);
+  zb = zs(nb);
+
+  dists = sqrt((xa-xb).^2 + (ya-yb).^2 + (za-zb).^2);
+  for i=1:length(na)
+      adj(na(i),nb(i))=dists(i);
   end
-  close(h);
+
