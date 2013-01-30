453636612b30c4eb718340160a769dfc4bbc74e6
Matthew Sottile <matt@galois.com>
2013-01-30 13:03:21 -0800

    missing

---
+++ code/splitter/splitter2.m
@@ -0,0 +1,11 @@
+%
+% code to split an image down the center and return the two halves
+% with the RHS registered with the LHS
+%
+function [lhs, rhsRegistered] = splitter2(im, tform)
+  [~,cols] = size(im);
+
+  lhs = im(:,1:(cols/2));
+  rhs = im(:,((cols/2)+1):end);
+
+  rhsRegistered = imtransform(rhs,tform);
