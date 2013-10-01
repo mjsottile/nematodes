f68398d27c6fbd7f8d07046cce3eb1efb117e9b2
Kat McCormick <mccormick@uoneuro.uoregon.edu>
2013-09-30 17:27:09 -0700

    this is messed up
    goodframe was only looking at the last 25% of frames, not the top 25%.
    Largest connected component is not assessed.

--- code/calcium/find_goodframe.m
+++ code/calcium/find_goodframe.m
@@ -4,14 +4,10 @@ function refframe = find_goodframe(ims, thresh, frac)
     
     % for each frame, compute the maximum pixel value and the
     % number of pixels in the connected component that is 
-    % within the threshold of the max.
+    % above threshold
     for i=1:numim
         maxval(i) = max(ims{i}(:));
-        
-%%% NOTE: CHANGED TO EXPERIMENT WITH ABSOLUTE THRESHOLDS
         masked = ims{i}(:) > thresh;
-%%%
-
         nummasked(i) = length(find(masked(:)));
     end
     
@@ -22,12 +18,20 @@ function refframe = find_goodframe(ims, thresh, frac)
     % assume frac is between 0 and 1.  A value like 0.25 is
     % ideal.
     
-    cutoff = round(numim * frac);
+    cutoff = numim - round(numim * frac);
+    
+    % Get the indicies of the top frac of frames with the most pixels above
+    % threshold. 
+    [pixsort, idx2] = sort(nummasked);
+    topidx = idx2(cutoff:end);
     
     % find the biggest connected component in the frames we consider
     % to be in the best frac% of the frames.
-    [~, idx2] = sort(nummasked((numim-cutoff):end));
+    %I can't figure out what this was supposed to be doing, but I
+    %think it was doing it wrong. KM
     
-    best = idx2(end)+(numim-cutoff-1);
-    refframe = idx(best);
+%     [~, idx2] = sort(nummasked((numim-cutoff):end));
+%     
+%     best = idx2(end)+(numim-cutoff-1);
+%     refframe = idx(best);
     
